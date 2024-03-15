1b6e4fc9295310769067357e5229033469189c08
AhmedBasionyHegazy <abhegazy123@gmail.com>
2024-03-15 20:48:29 +0200

    inerview solutions

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,56 +1,76 @@
 ﻿using LeetCode.WorkSpace;
 //// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+try
+{
+    Numbers numbers = new Numbers();
 
-Numbers numbers = new Numbers();
 
+    //while (true)
+    //{
+        //Console.WriteLine("enter number to check");
+        //int n = int.Parse(Console.ReadLine() ?? "");
 
-//while (true)
-//{
-    //Console.WriteLine("enter number to check");
-    //int n = int.Parse(Console.ReadLine() ?? "");
+        ////Palindrome Number
+        //Console.WriteLine(numbers.isPalindRome(n));
 
-    ////Palindrome Number
-    //Console.WriteLine(numbers.isPalindRome(n));
+        ////Trailing Zeros
+        //Console.WriteLine(numbers.Trailing_zeros(n));
 
-    ////Trailing Zeros
-    //Console.WriteLine(numbers.Trailing_zeros(n));
+        //Console.WriteLine("enter number n to check");
+        //int n = int.Parse(Console.ReadLine() ?? "");
 
-    //Console.WriteLine("enter number n to check");
-    //int n = int.Parse(Console.ReadLine() ?? "");
+        //Console.WriteLine("enter number x to check");
+        //double x = double.Parse(Console.ReadLine() ?? "");
 
-    //Console.WriteLine("enter number x to check");
-    //double x = double.Parse(Console.ReadLine() ?? "");
+        ////Pow(x,n)
+        //Console.WriteLine(numbers.myPow(x, n));
+    //}
 
-    ////Pow(x,n)
-    //Console.WriteLine(numbers.myPow(x, n));
-//}
+    Strings strings = new Strings();
 
-Strings strings = new Strings();
+    ////Revese String
+    //char[] chars = new char[]
+    //{
+    //    'h',
+    //    'e',
+    //    'l',
+    //    'l',
+    //    'o'
+    //};
+    //Console.WriteLine(strings.reveseString(chars));
 
-////Revese String
-//char[] chars = new char[]
-//{
-//    'h',
-//    'e',
-//    'l',
-//    'l',
-//    'o'
-//};
-//Console.WriteLine(strings.reveseString(chars));
+    ////Longest Common Prefix
+    //string[] strs = new string[] { "leetCode", "lead", "leet" };
+    //Console.WriteLine(strings.longestCommonPrefix(strs));
 
-////Longest Common Prefix
-//string[] strs = new string[] { "leetCode", "lead", "leet" };
-//Console.WriteLine(strings.longestCommonPrefix(strs));
+    ////Reverse Words In String
+    //string str = " Hello World ";
+    //Console.WriteLine(strings.reverseWords(str));
 
-////Reverse Words In String
-//string str = " Hello World ";
-//Console.WriteLine(strings.reverseWords(str));
+    ////longest Palindrome
+    //string str = "babad";
+    //Console.WriteLine(strings.longestPalindrome(str));
 
-////longest Palindrome
-//string str = "babad";
-//Console.WriteLine(strings.longestPalindrome(str));
+    ////length Of Longest Substring
+    //string str = "abcabcbb";
+    //Console.WriteLine(strings.lengthOfLongestSubstring(str));
 
-//length Of Longest Substring
-string str = "abcabcbb";
-Console.WriteLine(strings.lengthOfLongestSubstring(str));
+    //Test
+    Test test=new Test();
+    //Console.WriteLine("Enter String");
+    //string str = Console.ReadLine();
+    //Console.WriteLine(test.First_non_repeating_index(str));
+
+    //string[] s = new string[] { "eye", "california", "12321", "level", "was it a cat i saw" };
+    //string[] s =new string[] { "Dubai", "amman", "cairo", "fes" };
+    //Console.WriteLine(test.Longet_palindrome_string(s));
+
+    Console.WriteLine("Please Enter Number");
+    int num = int.Parse(Console.ReadLine());
+    Console.WriteLine(test.GetSum(num));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ LeetCode/WorkSpace/Test.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.WorkSpace
+{
+    public class Test
+    {
+        public int First_non_repeating_index(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return -1;
+            List<char> list = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        list.Add(s[i]);
+                        break;
+                    }
+                }
+                if (!list.Exists(x => x == s[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public string Longet_palindrome_string(string[] s)
+        {
+            if (s is null) return "-1";
+            int maxIndex = 0;
+            int maxLength = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (is_palindrome(s[i]) && s[i].Length > maxLength)
+                {
+                    maxIndex = i;
+                    maxLength = s[i].Length;
+                }
+            }
+            if (maxLength > 0)
+            {
+                return s[maxIndex];
+            }
+            return "-1";
+        }
+
+        private bool is_palindrome(string s)
+        {
+            if (s is null) return false;
+            //int endIndex = s.Length - 1;
+            //s = s.Trim();
+            s = s.ToLower();
+            s = string.Join("", s.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
+            int endIndex = s.Length - 1;
+            for (int i = 0; i < endIndex; i++)
+            {
+                //h:
+                //    if (s[i] == ' ')
+                //    {
+                //        i++;
+                //        goto h;
+                //    }
+                //y:
+                //    if (s[endIndex] == ' ')
+                //    {
+                //        endIndex--;
+                //        goto y;
+                //    }
+                if (s[i] != s[endIndex])
+                {
+                    return false;
+                }
+                endIndex--;
+            }
+            return true;
+        }
+
+        public string GetSum(int num)
+        {
+            if (num <= 0) return "";
+            int sum = 0;
+            List<int> nums=new List<int>();
+            while (num > 0)
+            {
+                nums.Insert(0,num % 10);
+                sum += num % 10;
+                num /= 10;
+            }
+            return string.Format($"{string.Join(" + ", nums)} = {sum}");
+        }
+    }
+}
